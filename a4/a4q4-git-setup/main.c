eba4068168ac71ec2394857e38be1c19598c5a5d
melkkkk <139573690+melkkkk@users.noreply.github.com>
2025-05-09 15:55:09 -0400

    Add files via upload

---
+++ a4/a4q4-git-setup/main.c
@@ -0,0 +1,13 @@
+
+#include "cs136-trace.h"
+#include <stdio.h>
+
+// This problem is more for helping you learn how to clone the git repo
+//   and use the makefile, rather than about C programming
+int main(void){
+  int val = 0;
+  while(1 == scanf("%d",&val)){
+    // uncomment the next line
+    printf("Integer read was: %d\n",val);
+  }
+} 
